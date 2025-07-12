249d8c5e70bc14c75b334d51d9d728218d53530a
n-viktor <viktorka20051014@gmail.com>
2025-07-12 15:33:59 +0200

    250712-2

--- headlines.js
+++ headlines.js
@@ -1,10 +1,10 @@
   // Your dynamic headlines (can come from an API or elsewhere)
   const headlines = [
+    "<a href='/cikkek/250712-szinte-valtozatlan-a-kulonbseg-a-republikon-szerint.html'>Republikon: 10 százalékkal vezet a Tisza</a>",
     "<a href='/cikkek/250711-realpr-kutatas-fidesz-vezet.html'>Real-PR: a Fidesz vezethet és kétpárti parlament alakulhat</a>",
     "<a href='/cikkek/250705-ketparti-parlament-lehet-a-tarsadalomkutato-szerint.html'>Kétpárti parlament lehet a Társadalomkutató szerint</a>",
     "<a href='/cikkek/250703-elmult-heti-kutatasok-osszefoglalo.html'>Legutóbbi kutatások: 4-ből 3 a Tisza előnyét mutatja</a>",
-    "<a href='/cikkek/250630-soha-nem-mert-elonye-lehet-a-tiszanak.html'>Soha nem mért előnyt jelez a 21 Kutatóközpont a TISZA számára</a>",
-    "<a href='/cikkek/250629-zavecz-11-szazalekkal-vezet-a-tisza.html'>Závecz Research: 11 százalékkal vezet a TISZA</a>"
+    "<a href='/cikkek/250630-soha-nem-mert-elonye-lehet-a-tiszanak.html'>Soha nem mért előnyt jelez a 21 Kutatóközpont a TISZA számára</a>"
   ];
 
   // Combine headlines into one scrolling string
