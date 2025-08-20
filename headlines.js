ed6ca5f208b591566e1024e22d2a86ec4efa3f85
n-viktor <viktorka20051014@gmail.com>
2025-08-20 11:23:45 +0200

    250820-1

--- headlines.js
+++ headlines.js
@@ -1,10 +1,9 @@
   // Your dynamic headlines (can come from an API or elsewhere)
   const headlines = [
+    "<a href='/cikkek/250820-idea-csokkent-a-tisza-elonye.html'>IDEA: Csökkent a Tisza előnye a teljes népesség körében</a>",
     "<a href='/cikkek/250812-publicus-tiz-szazalekkal-vezet-a-tisza.html'>Publicus: 10 százalékos a Tisza előnye a biztos pártválasztók körében</a>",
     "<a href='/cikkek/250801-republikon-nem-valtoztak-jelentosen-a-preferenciak.html'>Republikon: Nem változtak jelentősen a preferenciák</a>",
-    "<a href='/cikkek/250712-szinte-valtozatlan-a-kulonbseg-a-republikon-szerint.html'>Republikon: 10 százalékkal vezet a Tisza</a>",
-    "<a href='/cikkek/250711-realpr-kutatas-fidesz-vezet.html'>Real-PR: a Fidesz vezethet és kétpárti parlament alakulhat</a>",
-    "<a href='/cikkek/250705-ketparti-parlament-lehet-a-tarsadalomkutato-szerint.html'>Kétpárti parlament lehet a Társadalomkutató szerint</a>"
+    "<a href='/cikkek/250712-szinte-valtozatlan-a-kulonbseg-a-republikon-szerint.html'>Republikon: 10 százalékkal vezet a Tisza</a>"
   ];
 
   // Combine headlines into one scrolling string
