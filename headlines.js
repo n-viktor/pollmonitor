67a7a2a99695544415c986721648debd0d8a1206
n-viktor <viktorka20051014@gmail.com>
2026-02-03 22:01:29 +0100

    Update headlines.js

--- headlines.js
+++ headlines.js
@@ -1,11 +1,11 @@
   // Your dynamic headlines (can come from an API or elsewhere)
   const headlines = [
+    "<a href='/kutatok/21kutato.html'>21 KUTATÓKÖZPONT: TISZA 53% - 37% FIDESZ</a>",
     "<a href='/kutatok/publicus.html'>PUBLICUS: TISZA 48% - 40% FIDESZ</a>",
     "MINERVA: TISZA 54,3% - 36,7% FIDESZ",
     "<a href='/kutatok/republikon.html'>REPUBLIKON: TISZA 48% - 37% FIDESZ</a>",
     "XXI. SZÁZAD INTÉZET: FIDESZ 45% - 40% TISZA",
-    "<a href='/kutatok/median.html'>MEDIÁN: TISZA 51% - 39% FIDESZ</a>",
-    "<a href='/kutatok/idea.html'>IDEA: TISZA 48% - 38% FIDESZ</a>",
+    "<a href='/kutatok/median.html'>MEDIÁN: TISZA 51% - 39% FIDESZ</a>"
   ];
 
   // Combine headlines into one scrolling string
