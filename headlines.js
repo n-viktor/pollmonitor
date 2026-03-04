257386903efc6cd52e92e025a4e135138d547310
n-viktor <viktorka20051014@gmail.com>
2026-03-04 16:11:55 +0100

    Update headlines.js

--- headlines.js
+++ headlines.js
@@ -1,10 +1,11 @@
   // Your dynamic headlines (can come from an API or elsewhere)
   const headlines = [
+    "<a href='/kutatok/zavecz.html'>ZÁVECZ: TISZA 50% - 38% FIDESZ</a>",
+    "<a href='/kutatok/nezopont.html'>NÉZŐPONT: TISZA 40% - 45% FIDESZ</a>",
+    "<a href='/kutatok/publicus.html'>PUBLICUS: TISZA 47% - 39% FIDESZ</a>",
     "<a href='/kutatok/median.html'>MEDIÁN: TISZA 55% - 35% FIDESZ</a>",
     "<a href='/kutatok/republikon.html'>REPUBLIKON: TISZA 47% - 39% FIDESZ</a>",
     "<a href='/kutatok/iranytu.html'>IRÁNYTŰ: TISZA 50% - 37% FIDESZ</a>",
-    "<a href='/kutatok/publicus.html'>PUBLICUS: TISZA 48% - 39% FIDESZ</a>",
-    "<a href='/kutatok/nezopont.html'>NÉZŐPONT: TISZA 40% - 46% FIDESZ</a>",
     "<a href='/kutatok/idea.html'>IDEA: TISZA 48% - 38% FIDESZ</a>"
   ];
 
