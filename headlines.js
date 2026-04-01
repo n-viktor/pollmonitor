fde3067b2e5c470b51e7565c12d27232a1b6ea64
n-viktor <viktorka20051014@gmail.com>
2026-04-01 09:05:29 +0200

    04-01

--- headlines.js
+++ headlines.js
@@ -1,14 +1,14 @@
   // Your dynamic headlines (can come from an API or elsewhere)
   const headlines = [
+    "<a href='/kutatok/21kutato.html'>21 KUTATÓKÖZPONT: TISZA 56% - 37% FIDESZ</a>",
+    "<a href='/kutatok/zavecz.html'>ZÁVECZ: TISZA 51% - 38% FIDESZ</a>",
+    "<a href='/kutatok/republikon.html'>REPUBLIKON: TISZA 49% - 40% FIDESZ</a>",
     "<a href='/kutatok/median.html'>MEDIÁN: TISZA 58% - 35% FIDESZ</a>",
     "<a href='/kutatok/nezopont.html'>NÉZŐPONT: TISZA 40% - 46% FIDESZ</a>",
     "MINERVA: TISZA 54% - 38% FIDESZ",
     "XXI. SZÁZAD INT.: TISZA 41% - 46% FIDESZ",
     "<a href='/kutatok/idea.html'>IDEA: TISZA 49% - 37% FIDESZ</a>",
-    "<a href='/kutatok/21kutato.html'>21 KUTATÓKÖZPONT: TISZA 53% - 39% FIDESZ</a>",
-    "<a href='/kutatok/zavecz.html'>ZÁVECZ: TISZA 50% - 38% FIDESZ</a>",
     "<a href='/kutatok/publicus.html'>PUBLICUS: TISZA 47% - 39% FIDESZ</a>",
-    "<a href='/kutatok/republikon.html'>REPUBLIKON: TISZA 47% - 39% FIDESZ</a>",
     "<a href='/kutatok/iranytu.html'>IRÁNYTŰ: TISZA 50% - 37% FIDESZ</a>"
   ];
 
